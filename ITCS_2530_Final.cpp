9c8c8e932947679ca16f20fa2677adb67dc90489
Josh <quintanoj339@macomb.edu>
2022-12-15 11:14:29 -0500

    wrote skeleton of the main menu program.

--- ITCS_2530_Final.cpp
+++ ITCS_2530_Final.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -20,6 +21,7 @@ struct patronRecord
     int ID;
     string firstName;
     string lastName;
+    
 };
 
 //Stubs
@@ -35,6 +37,62 @@ void checkAccount(bookRecord list[]);
 const int BOOK_ARRAY_SIZE = 100;
 const int PATRON_ARRAY_SIZE = 100;
 
+//functions
+void PrintMenu()
+{
+    cout << setw(28) << setfill('*') << '*' << endl;
+    cout << setw(5) << setfill('*') << '*' << " Library Services " << setw(5) << setfill('*') << '*' << endl;
+    cout << setw(28) << setfill('*') << '*' << endl << endl;
+
+    string input;
+    cout << "Add book to book list(AB)." << endl;
+    cout << "Delete book from booklist(DB)." << endl;
+    cout << "Add patron to patron list(AP)." << endl;
+    cout << "Delete patron from patron list(DP)." << endl;
+    cout << "Borrow book(BB)." << endl;
+    cout << "Return book(RB)." << endl;
+    cout << "Check account(CA)." << endl << endl;
+
+    while (input != "q") 
+    {  
+        cout << "Please make a selection: ";
+        cin >> input;
+        if (input == "AB")
+        {         
+            cout << "check";
+        }
+        else if (input == "DB")
+        {
+            cout << "check";
+        }
+        else if (input == "AP")
+        {
+            cout << "check";
+        }
+        else if (input == "DP")
+        {
+            cout << "check";
+        }
+        else if (input == "BB")
+        {
+            cout << "check";
+        }
+        else if (input == "RB") 
+        {
+            cout << "check";
+        }
+        else if (input == "CA")
+        {
+            cout << "check";
+        }
+        else {
+            cout << "Thank you!";
+            exit(0);
+        }
+        
+    }   
+    
+}
 //Main
 int main()
 {
@@ -45,9 +103,11 @@ int main()
     patronRecord patronList[PATRON_ARRAY_SIZE];
     bookRecord patronAccount[BOOK_ARRAY_SIZE];
     
-
+    //function calls
+    PrintMenu();
     return 0;
 }
+
 
 void addBook(bookRecord list[])
 {
